64fe1404051e930032aaa2518c04e2c37c7e80bd
Jackson Redman <jacksonr121@outlook.com>
2026-02-11 02:20:41 -0500

    imports and fixes

--- crates/relayrl_algorithms/src/algorithms/REINFORCE/replay_buffer.rs
+++ crates/relayrl_algorithms/src/algorithms/REINFORCE/replay_buffer.rs
@@ -1,15 +1,24 @@
-;6use crate::templates::base_replay_buffer::*;
+use crate::templates::base_replay_buffer::{BufferTensors, BatchKey, BufferSample, SampleScalars, Batch, GenericReplayBuffer, ReplayBufferError};
 
+use relayrl_types::prelude::trajectory::RelayRLTrajectory;
+use relayrl_types::prelude::action::RelayRLData;
+use relayrl_types::prelude::tensor::relayrl::{TensorData, BackendMatcher};
+
+use burn_tensor::backend::Backend;
 use tokio::sync::Mutex;
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::collections::HashMap;
 
 struct Buffers {
     observations: BufferTensors,
     actions: BufferTensors,
-    mask: BufferTensors,
+    masks: BufferTensors,
     rewards: Vec<f32>,
     advantages: BufferTensors,
     returns: Vec<f32>,
-    logprob: BufferTensors,
+    logprobs: BufferTensors,
     values: Option<BufferTensors>,
 }
 
@@ -27,8 +36,8 @@ pub struct ReinforceReplayBuffer {
     metadata: Arc<BufferMetadata>
 }
 
-impl REINFORCEReplayBuffer {
-    fn new(buffer_size: usize, gamma: f32, lambda: f32, with_vf_baseline: bool) -> Self {
+impl ReinforceReplayBuffer {
+    pub fn new(buffer_size: usize, gamma: f32, lambda: f32, with_vf_baseline: bool) -> Self {
         let buffers = Buffers {
             observations: BufferTensors::new(),
             actions: BufferTensors::new(),
@@ -38,31 +47,31 @@ impl REINFORCEReplayBuffer {
             returns: Vec::<f32>::new(),
             logprobs: BufferTensors::new(),
             values: {
-                match self.with_vf_baseline {
+                match with_vf_baseline {
                     true => Some(BufferTensors::new()),
                     false => None,
                 }
             }
-        }
+        };
 
         Self {
-            buffers,
-            metadata: BufferMetadata {
+            buffers: Arc::new(Mutex::new(buffers)),
+            metadata: Arc::new(BufferMetadata {
                 gamma,
                 lambda,
                 with_vf_baseline,
                 buffer_size,
                 buffer_pointer: AtomicUsize::new(0),
                 buffer_path_start_idx: AtomicUsize::new(0)
-            }
+            }),
         }
 
     }
 
     async fn compute_path_end(&self, final_value: Option<f32>) {
-        let buffer_lock = self.buffer.lock().await?;
+        let buffer_lock = self.buffers.lock().await;
 
-        let final_value.unwrap_or(0);
+        let final_value = final_value.unwrap_or(0.0);
 
         let start = self.metadata.buffer_path_start_idx.load(Ordering::SeqCst);
         let end = self.metadata.buffer_pointer.load(Ordering::SeqCst);
@@ -80,7 +89,7 @@ impl REINFORCEReplayBuffer {
             let advantages = discounted_cumsum(&deltas, self.metadata.gamma * self.metadata.lambda)?;
             buffer_lock.advantages[slice.clone()].copy_from_slice(&advantages);
         } else {
-            let rewards = buffer_writer.rewards[slice.clone()];
+            let rewards = buffer_lock.rewards[slice.clone()];
 
             let advantages = discounted_cumsum(&rewards, self.metadata.gamma)?;
             buffer_lock.advantages[slice.clone()].copy_from_slice(&advantages);
@@ -93,58 +102,58 @@ impl REINFORCEReplayBuffer {
     }
 }
 
-impl<B: Backend + BackendMatcher<Backend = B>> GenericReplayBuffer<B> for REINFORCEReplayBuffer {
+impl<B: Backend + BackendMatcher<Backend = B>> GenericReplayBuffer<B> for ReinforceReplayBuffer {
     async fn insert_trajectory(&self, trajectory: RelayRLTrajectory) -> Result<(), ReplayBufferError> {
         let buffer_lock = self.buffer.lock().await?;
         for action in trajectory.actions.iter() {
-            match action.obs {
+            match action.get_obs() {
                 Some(obs) => {
                     buffer_lock.observations.push(Some(obs));
                 },
-                None = buffer_lock.observations.push(None);
+                None => buffer_lock.observations.push(None),
             };
 
-            match action.act {
+            match action.get_act() {
                 Some(act) => {
                     buffer_lock.actions.push(Some(act));
                 },
-                None => buffer_lock.actions.push(None);
+                None => buffer_lock.actions.push(None),
             };
 
-            match action.mask {
+            match action.get_mask() {
                 Some(mask) => {
                     buffer_lock.masks.push(Some(mask));
                 },
-                None => buffer_lock.masks.push(None);
+                None => buffer_lock.masks.push(None),
             };
 
-            buffer_writer.rewards.push(action.rew);
+            buffer_lock.rewards.push(action.get_rew());
 
-            match action.data {
+            match action.get_data() {
                 Some(data_map) => {
                     match data_map.get("logp_a") {
                         Some(data) => {
                                 match data {
-                                    Tensor(logp_a) => {
+                                    RelayRLData::Tensor(logp_a) => {
                                         buffer_lock.logprobs.push(Some(logp_a));
                                     },
-                                    _ => Return Err(ReplayBufferError::TrajectoryInsertionError("`LogProb` expected to be RelayRLData::Tensor"));
+                                    _ => return Err(ReplayBufferError::TrajectoryInsertionError("`LogProb` expected to be RelayRLData::Tensor".to_string())),
                                 }
                         },
-                        None => buffer_lock.logprobs.push(None);
+                        None => buffer_lock.logprobs.push(None),
                     }
 
                     if self.with_vf_baseline {
                         match data_map.get("val") {
                             Some(data) => {
                                 match data {
-                                    Tensor(val) => {
-                                        buffer_lock.values.push(val);
+                                    RelayRLData::Tensor(val) => {
+                                        buffer_lock.values.push(Some(val));
                                     },
-                                    _ => Return Err(ReplayBufferError::TrajectoryInsertionError("`Val` expected to be RelayRLData::Tensor"));
+                                    _ => return Err(ReplayBufferError::TrajectoryInsertionError("`Val` expected to be RelayRLData::Tensor".to_string())),
                                 }
                             },
-                            None => buffer_lock.values.push(None);
+                            None => buffer_lock.values.push(None),
                         }
                     }
                 },
@@ -152,7 +161,7 @@ impl<B: Backend + BackendMatcher<Backend = B>> GenericReplayBuffer<B> for REINFO
                     buffer_lock.logprobs.push(None);
 
                     if self.with_vf_baseline {
-                        buffer_writer.values.push(None);
+                        buffer_lock.values.push(None);
                     }
                 }
             };
@@ -166,11 +175,11 @@ impl<B: Backend + BackendMatcher<Backend = B>> GenericReplayBuffer<B> for REINFO
     async fn sample_buffer<const N: usize>(&self) -> Result<Batch, ReplayBufferError> {
         assert!(self.metadata.buffer_pointer < self.metadata.buffer_size);
 
-        let buffer_lock = self.buffer.lock().await?;
+        let buffer_lock = self.buffers.lock().await;
 
-        let capacity = self.metadata.buffer_pointer(load(Ordering::SeqCst));
+        let capacity = self.metadata.buffer_pointer.load(Ordering::SeqCst);
         self.metadata.buffer_pointer.store(0, Ordering::SeqCst);
-        self.metadata.buffer_path_start_idx(0, Ordering::SeqCst);
+        self.metadata.buffer_path_start_idx.store(0, Ordering::SeqCst);
 
         let boxed_adv: Box<[TensorData]> = {
             let adv_buffer = &buffer_lock.advantages;
@@ -216,16 +225,16 @@ impl<B: Backend + BackendMatcher<Backend = B>> GenericReplayBuffer<B> for REINFO
             let logp_at_capacity = logp_buffer[..capacity];
 
             logp_at_capacity.to_vec().to_boxed_slice()
-        }
+        };
 
-        let batch = Batch::new();
+        let mut batch: HashMap<BatchKey, BufferSample> = HashMap::new();
 
         batch.insert(BatchKey::Obs, BufferSample::Tensors(boxed_obs));
         batch.insert(BatchKey::Act, BufferSample::Tensors(boxed_act));
         batch.insert(BatchKey::Mask, BufferSample::Tensors(boxed_mask));
-        batch.insert(BatchKey::Custom("Adv"), BufferSample::Tensors(boxed_adv));
-        batch.insert(BatchKey::Custom("Ret"), BufferSample::Scalars(SampleScalars::F32(boxed_ret)));
-        batch.insert(BatchKey::Custom("LogP"), BufferSample::Tensors(boxed_logp));
+        batch.insert(BatchKey::Custom("Adv".to_string()), BufferSample::Tensors(boxed_adv));
+        batch.insert(BatchKey::Custom("Ret".to_string()), BufferSample::Scalars(SampleScalars::F32(boxed_ret)));
+        batch.insert(BatchKey::Custom("LogP".to_string()), BufferSample::Tensors(boxed_logp));
 
         Ok(batch)
     }
