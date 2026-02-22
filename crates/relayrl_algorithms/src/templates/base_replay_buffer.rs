c3d91c1c8883911d7996d7e395333add282c62d8
Jackson Redman <jacksonr121@outlook.com>
2026-02-21 21:44:38 -0500

    removed unused imports

--- crates/relayrl_algorithms/src/templates/base_replay_buffer.rs
+++ crates/relayrl_algorithms/src/templates/base_replay_buffer.rs
@@ -1,19 +1,18 @@
 
-use relayrl_types::prelude::data::trajectory::RelayRLTrajectory;
-use relayrl_types::prelude::data::tensor::relayrl::{AnyBurnTensor, TensorData};
+use relayrl_types::prelude::trajectory::RelayRLTrajectory;
+use relayrl_types::prelude::tensor::relayrl::TensorData;
 
-use std::sync::Arc;
-use std::collections::{HashMap, HashSet};
-use tokio::sync::RwLock;
-use dashmap::DashMap;
+use std::boxed::Box;
+use std::collections::HashMap;
 use thiserror::Error;
 use async_trait::async_trait;
+use std::any::Any;
 
 #[derive(Clone, Debug, Error)]
 pub enum ReplayBufferError {
-    #[error("Insertion of trajectory failed: {}")]
+    #[error("Insertion of trajectory failed: {0}")]
     TrajectoryInsertionError(String),
-    #[error("Buffer sampling failed: {}")]
+    #[error("Buffer sampling failed: {0}")]
     BufferSamplingError(String),
 }
 
@@ -46,16 +45,6 @@ pub type Batch = HashMap<BatchKey, BufferSample>;
 
 #[async_trait]
 pub trait GenericReplayBuffer: Send + Sync {
-    async fn insert_trajectory(&self, trajectory: RelayRLTrajectory) -> Result<(), ReplayBufferError>;
+    async fn insert_trajectory(&self, trajectory: RelayRLTrajectory) -> Result<Box<dyn Any>, ReplayBufferError>;
     async fn sample_buffer(&self) -> Result<Batch, ReplayBufferError>;
-}
-
-pub fn discounted_cumsum(x: &[f32], discounted: f32) {
-
-}
-
-pub fn scalar_stats(x: &[])
-
-pub fn compute_normed_advantages(advantages: &[f32], mean: f32, std: f32) -> &[f32] {
-
 }
