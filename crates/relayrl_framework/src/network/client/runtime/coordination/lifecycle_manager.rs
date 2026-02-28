c4b726c71aba0ab24652ad63b80d368ea7cb400b
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2026-02-28 22:55:27 +0800

    updated addressing to reflect lack of changeable prefix, renamed server_addresses to transport_addresses

--- crates/relayrl_framework/src/network/client/runtime/coordination/lifecycle_manager.rs
+++ crates/relayrl_framework/src/network/client/runtime/coordination/lifecycle_manager.rs
@@ -1,19 +1,19 @@
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::network::HyperparameterArgs;
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::network::TransportType;
-use crate::network::client::agent::TrajectoryFileParams;
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+use crate::network::client::agent::LocalTrajectoryFileParams;
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::network::client::runtime::coordination::scale_manager::AlgorithmArgs;
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::prelude::config::TransportConfigParams;
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::utilities::configuration::Algorithm;
 use crate::utilities::configuration::{ClientConfigLoader, LocalModelModuleParams};
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use crate::utilities::configuration::{HyperparameterConfig, NetworkParams};
 
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
 use std::collections::HashMap;
 use std::env;
 use std::fs;
@@ -24,57 +24,93 @@ use tokio::sync::{Notify, RwLock, broadcast};
 
 use thiserror::Error;
 
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-#[derive(Debug, Clone)]
-pub(crate) struct ServerAddresses {
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct SharedInferenceAddresses {
     pub(crate) inference_server_address: Arc<str>,
+    pub(crate) inference_scaling_server_address: Arc<str>,
+}
+
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct SharedTrainingAddresses {
     pub(crate) agent_listener_address: Arc<str>,
     pub(crate) model_server_address: Arc<str>,
     pub(crate) trajectory_server_address: Arc<str>,
-    pub(crate) scaling_server_address: Arc<str>,
+    pub(crate) training_scaling_server_address: Arc<str>,
 }
 
-#[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-pub(crate) fn construct_server_addresses(
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct SharedTransportAddresses {
+    pub(crate) inference_addresses: SharedInferenceAddresses,
+    pub(crate) training_addresses: SharedTrainingAddresses,
+}
+
+#[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+pub(crate) fn construct_transport_addresses(
     transport_config: &TransportConfigParams,
     transport_type: &TransportType,
-) -> ServerAddresses {
+) -> SharedTransportAddresses {
     fn construct_address(
         transport_type: &TransportType,
         network_params: &NetworkParams,
     ) -> Arc<str> {
         match *transport_type {
-            #[cfg(feature = "zmq_transport")]
+            #[cfg(feature = "zmq-transport")]
             TransportType::ZMQ => Arc::<str>::from(
-                network_params.prefix.clone()
+                "tcp://".to_owned()
                     + &network_params.host.clone()
                     + ":"
-                    + &network_params.port.clone(),
+                    + &network_params.port.clone().to_string(),
+            ),
+            #[cfg(feature = "nats-transport")]
+            TransportType::NATS => Arc::<str>::from(
+                "nats://".to_owned()
+                    + &network_params.host.clone()
+                    + ":"
+                    + &network_params.port.clone().to_string(),
             ),
         }
     }
 
-    ServerAddresses {
-        inference_server_address: construct_address(
-            transport_type,
-            &transport_config.inference_server_address,
-        ),
-        agent_listener_address: construct_address(
-            transport_type,
-            &transport_config.agent_listener_address,
-        ),
-        model_server_address: construct_address(
-            transport_type,
-            &transport_config.model_server_address,
-        ),
-        trajectory_server_address: construct_address(
-            transport_type,
-            &transport_config.trajectory_server_address,
-        ),
-        scaling_server_address: construct_address(
-            transport_type,
-            &transport_config.scaling_server_address,
-        ),
+    SharedTransportAddresses {
+        inference_addresses: SharedInferenceAddresses {
+            inference_server_address: construct_address(
+                transport_type,
+                &transport_config
+                    .inference_addresses
+                    .inference_server_address,
+            ),
+            inference_scaling_server_address: construct_address(
+                transport_type,
+                &transport_config
+                    .inference_addresses
+                    .inference_scaling_server_address,
+            ),
+        },
+        training_addresses: SharedTrainingAddresses {
+            agent_listener_address: construct_address(
+                transport_type,
+                &transport_config.training_addresses.agent_listener_address,
+            ),
+            model_server_address: construct_address(
+                transport_type,
+                &transport_config.training_addresses.model_server_address,
+            ),
+            trajectory_server_address: construct_address(
+                transport_type,
+                &transport_config
+                    .training_addresses
+                    .trajectory_server_address,
+            ),
+            training_scaling_server_address: construct_address(
+                transport_type,
+                &transport_config
+                    .training_addresses
+                    .training_scaling_server_address,
+            ),
+        },
     }
 }
 
@@ -86,15 +122,14 @@ pub(crate) fn construct_local_model_path(local_model_module: &LocalModelModulePa
 }
 
 pub(crate) fn construct_trajectory_file_output(
-    trajectory_file_output: &TrajectoryFileParams,
-) -> TrajectoryFileParams {
+    trajectory_file_output: &LocalTrajectoryFileParams,
+) -> LocalTrajectoryFileParams {
     let cwd: PathBuf = env::current_dir().unwrap_or_else(|_| PathBuf::from("."));
     let directory = cwd.join(&trajectory_file_output.directory);
 
-    TrajectoryFileParams {
-        enabled: trajectory_file_output.enabled,
-        encode: trajectory_file_output.encode,
+    LocalTrajectoryFileParams {
         directory,
+        file_type: trajectory_file_output.file_type.clone(),
     }
 }
 
@@ -117,16 +152,16 @@ pub enum LifeCycleManagerError {
 /// Spins up and tears down futures cleanly
 #[derive(Debug, Clone)]
 pub(crate) struct LifeCycleManager {
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
     algorithm_args: Arc<AlgorithmArgs>,
     max_traj_length: Arc<RwLock<u128>>,
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-    server_addresses: Arc<RwLock<ServerAddresses>>,
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+    transport_addresses: Arc<RwLock<SharedTransportAddresses>>,
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
     init_hyperparameters: Arc<RwLock<HashMap<Algorithm, HyperparameterArgs>>>,
     local_model_path: Arc<RwLock<PathBuf>>,
-    trajectory_file_output: Arc<RwLock<TrajectoryFileParams>>,
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    trajectory_file_output: Arc<RwLock<LocalTrajectoryFileParams>>,
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
     transport_type: Arc<TransportType>,
     config_path: Arc<PathBuf>,
     config_update_polling_seconds: Arc<RwLock<f32>>,
@@ -136,12 +171,12 @@ pub(crate) struct LifeCycleManager {
 }
 
 impl LifeCycleManager {
-    pub fn new(
-        #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    pub(crate) fn new(
+        #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
         algorithm_args: AlgorithmArgs,
-        config: ClientConfigLoader,
+        config: &ClientConfigLoader,
         config_path: PathBuf,
-        #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+        #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
         transport_type: TransportType,
     ) -> Self {
         let (shutdown_tx, _) = broadcast::channel(10_000);
@@ -163,20 +198,20 @@ impl LifeCycleManager {
         let max_traj_length = transport_config.max_traj_length.clone();
 
         Self {
-            #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+            #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
             algorithm_args: Arc::new(algorithm_args),
             max_traj_length: Arc::new(RwLock::new(max_traj_length)),
-            #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-            server_addresses: Arc::new(RwLock::new(construct_server_addresses(
-                &config.transport_config,
+            #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+            transport_addresses: Arc::new(RwLock::new(construct_transport_addresses(
+                &transport_config,
                 &transport_type,
             ))),
-            #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+            #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
             init_hyperparameters: Arc::new(RwLock::new(
                 config.client_config.init_hyperparameters.to_args(None),
             )),
             local_model_path: Arc::new(RwLock::new(construct_local_model_path(
-                &config.transport_config.local_model_module,
+                &transport_config.local_model_module,
             ))),
             trajectory_file_output: Arc::new(RwLock::new(construct_trajectory_file_output(
                 &config.client_config.trajectory_file_output,
@@ -185,14 +220,14 @@ impl LifeCycleManager {
             last_modified: Arc::new(RwLock::new(last_modified)),
             config_update_polling_seconds: Arc::new(RwLock::new(config_update_polling)),
             shutdown_tx,
-            #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+            #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
             transport_type: Arc::new(transport_type),
             shutdown_notifier: Arc::new(Notify::new()),
         }
     }
 
     // Listen for shutdown signals and config changes
-    pub fn spawn_loop(&self) {
+    pub(crate) fn spawn_loop(&self) {
         let self_clone: LifeCycleManager = self.clone();
         tokio::spawn(async move {
             if let Err(e) = self_clone._watch().await {
@@ -209,30 +244,30 @@ impl LifeCycleManager {
         self.max_traj_length.clone()
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-    pub fn get_server_addresses(&self) -> Arc<RwLock<ServerAddresses>> {
-        self.server_addresses.clone()
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+    pub fn get_transport_addresses(&self) -> Arc<RwLock<SharedTransportAddresses>> {
+        self.transport_addresses.clone()
     }
 
     pub fn get_local_model_path(&self) -> Arc<RwLock<PathBuf>> {
         self.local_model_path.clone()
     }
 
-    pub fn get_trajectory_file_output(&self) -> Arc<RwLock<TrajectoryFileParams>> {
+    pub fn get_trajectory_file_output(&self) -> Arc<RwLock<LocalTrajectoryFileParams>> {
         self.trajectory_file_output.clone()
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
     pub fn get_init_hyperparameters(&self) -> Arc<RwLock<HashMap<Algorithm, HyperparameterArgs>>> {
         self.init_hyperparameters.clone()
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-    pub fn get_algorithm_args(&self) -> Arc<AlgorithmArgs> {
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+    pub(crate) fn get_algorithm_args(&self) -> Arc<AlgorithmArgs> {
         self.algorithm_args.clone()
     }
 
-    pub async fn set_max_traj_length(
+    pub(crate) async fn set_max_traj_length(
         &self,
         max_traj_length: &u128,
     ) -> Result<(), LifeCycleManagerError> {
@@ -241,20 +276,21 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-    pub async fn set_server_addresses(
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+    pub(crate) async fn set_transport_addresses(
         &self,
         transport_params: &TransportConfigParams,
-        #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+        #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
         transport_type: &TransportType,
     ) -> Result<(), LifeCycleManagerError> {
-        let mut server_addresses_guard = self.server_addresses.write().await;
-        *server_addresses_guard = construct_server_addresses(transport_params, transport_type);
+        let mut transport_addresses_guard = self.transport_addresses.write().await;
+        *transport_addresses_guard =
+            construct_transport_addresses(transport_params, transport_type);
         Ok(())
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
-    pub async fn set_init_hyperparameters(
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
+    pub(crate) async fn set_init_hyperparameters(
         &self,
         init_hyperaparameters: &HyperparameterConfig,
     ) -> Result<(), LifeCycleManagerError> {
@@ -264,7 +300,7 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    pub async fn set_local_model_path(
+    pub(crate) async fn set_local_model_path(
         &self,
         local_model_module: &LocalModelModuleParams,
     ) -> Result<(), LifeCycleManagerError> {
@@ -273,21 +309,21 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    pub async fn set_trajectory_file_path(
+    pub(crate) async fn set_trajectory_file_path(
         &self,
-        trajectory_file_output: &TrajectoryFileParams,
+        trajectory_file_output: &LocalTrajectoryFileParams,
     ) -> Result<(), LifeCycleManagerError> {
         let mut trajectory_file_output_guard = self.trajectory_file_output.write().await;
         *trajectory_file_output_guard = construct_trajectory_file_output(trajectory_file_output);
         Ok(())
     }
 
-    pub fn _shutdown(&mut self) -> Result<(), LifeCycleManagerError> {
+    pub(crate) fn _shutdown(&mut self) -> Result<(), LifeCycleManagerError> {
         self.shutdown_notifier.notify_waiters();
         self._handle_shutdown_signal()
     }
 
-    async fn _watch(&self) -> Result<(), LifeCycleManagerError> {
+    pub(crate) async fn _watch(&self) -> Result<(), LifeCycleManagerError> {
         loop {
             let config_update_polling_seconds =
                 self.config_update_polling_seconds.read().await.clone() as u64;
@@ -320,7 +356,7 @@ impl LifeCycleManager {
         }
     }
 
-    fn _handle_shutdown_signal(&self) -> Result<(), LifeCycleManagerError> {
+    pub(crate) fn _handle_shutdown_signal(&self) -> Result<(), LifeCycleManagerError> {
         if let Err(e) = self.shutdown_tx.send(()) {
             return Err(LifeCycleManagerError::SendShutdownSignalError(
                 format!("Failed to send shutdown signal. No active receivers: {}", e).to_string(),
@@ -329,7 +365,7 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    #[cfg(any(feature = "async_transport", feature = "sync_transport"))]
+    #[cfg(any(feature = "nats-transport", feature = "zmq-transport"))]
     pub(crate) async fn _handle_config_change(
         &self,
         path: PathBuf,
@@ -338,7 +374,7 @@ impl LifeCycleManager {
 
         tokio::try_join!(
             self.set_max_traj_length(&new_config.transport_config.max_traj_length),
-            self.set_server_addresses(&new_config.transport_config, &self.transport_type),
+            self.set_transport_addresses(&new_config.transport_config, &self.transport_type),
             self.set_local_model_path(&new_config.transport_config.local_model_module),
             self.set_trajectory_file_path(&new_config.client_config.trajectory_file_output),
             self.set_init_hyperparameters(&new_config.client_config.init_hyperparameters),
@@ -350,7 +386,7 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    #[cfg(not(any(feature = "async_transport", feature = "sync_transport")))]
+    #[cfg(not(any(feature = "nats-transport", feature = "zmq-transport")))]
     pub(crate) async fn _handle_config_change(
         &self,
         path: PathBuf,
@@ -369,7 +405,12 @@ impl LifeCycleManager {
         Ok(())
     }
 
-    pub fn subscribe_shutdown(&self) -> Result<broadcast::Receiver<()>, LifeCycleManagerError> {
+    pub(crate) fn subscribe_shutdown(
+        &self,
+    ) -> Result<broadcast::Receiver<()>, LifeCycleManagerError> {
         Ok(self.shutdown_tx.subscribe())
     }
 }
+
+#[cfg(test)]
+mod tests {}
