16344de0c48b3603fc731bde92739daa3b5e1813
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2026-03-07 01:40:26 +0800

    prep for NATS impl in the next update

--- crates/relayrl_framework/src/network/client/runtime/data/transport_sink/nats/mod.rs
+++ crates/relayrl_framework/src/network/client/runtime/data/transport_sink/nats/mod.rs
@@ -1,2 +1,18 @@
+pub(crate) mod interface;
+pub(super) mod ops;
+pub(super) mod policies;
+
+#[derive(Debug, Error, Clone)]
+pub enum NatsClientError {
+    #[error("NATS transport error: {0}")]
+    NatsTransportError(String),
+    #[error("Task join error: {0}")]
+    JoinError(String),
+}
+
+pub(super) trait NatsInferenceExecution {}
+
+pub(super) trait NatsTrainingExecution {}
+
 #[cfg(test)]
 mod tests {}
