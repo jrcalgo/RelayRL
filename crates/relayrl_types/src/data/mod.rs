1962c3435cefbdae3dfb0441215b6f9704d43f17
Jackson Redman <jacksonr121@outlook.com>
2026-03-29 17:37:11 -0400

    resolved `inference-models` feature flag related warnings

--- crates/relayrl_types/src/data/mod.rs
+++ crates/relayrl_types/src/data/mod.rs
@@ -1,7 +1,11 @@
+#[cfg(any(feature = "ndarray-backend", feature = "tch-backend"))]
 pub mod action;
+#[cfg(any(feature = "ndarray-backend", feature = "tch-backend"))]
 pub mod tensor;
+#[cfg(any(feature = "ndarray-backend", feature = "tch-backend"))]
 pub mod trajectory;
 
+#[cfg(any(feature = "ndarray-backend", feature = "tch-backend"))]
 pub mod records;
 
 pub mod utilities {
